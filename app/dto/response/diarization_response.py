ce961ca30e3f6c440c151d554120a16c6d38a6f6
SangJung <sangjung245@gmail.com>
2025-05-30 22:05:09 +0000

    [FEAT] diarization 응답에 group_id 추가

--- app/dto/response/diarization_response.py
+++ app/dto/response/diarization_response.py
@@ -5,12 +5,14 @@ from services.rt_diarization import RTDiarizationOutput
 from .sentence import Sentence
 
 class DiarizationResponse(BaseModel):
+    group_id: str
     completed: list[Sentence]
     candidate: list[Sentence]
 
     @staticmethod
     def from_rt_diarization_output(Y: RTDiarizationOutput):
         return DiarizationResponse(
+            group_id=Y.group_id,
             completed=[Sentence.get_from_speak(speak) for speak in Y.completed],
             candidate=[Sentence.get_from_speak(speak) for speak in Y.candidate],
         )
